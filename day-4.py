7a82db4635b728e923e351dc0bd1f1eb644caf16
Malik Shair Khan <100118245+malikshairkhan@users.noreply.github.com>
2024-07-18 16:10:42 +0500

    Add files via upload

---
+++ day-4.py
@@ -0,0 +1,46 @@
+"""ls = [1,2,2,4,5,6,7,8,9]
+for i in ls:
+    print(i)
+    
+"""
+"""x = 0
+while(x < len(ls)):
+    print(ls[x])
+    x += 1"""
+"""    
+while ls:
+    list = ls.pop()
+    print(list)
+    """
+    
+"""for x in range(4,41,4):
+    print(x)"""
+
+"""user_input = ""
+while user_input.lower() != "quit":
+    user_input = input("Enter something (or 'quit' to stop): ")
+    print(f"You entered: {user_input}")"""
+    
+    
+    
+
+"""string = "basit"
+
+reversed_string = ""
+index = len(string) -1
+
+while index >= 0:
+    reversed_string += string[index]
+    index -= 1
+print(f"reversed stirng is:{reversed_string}")"""
+
+
+
+""""list = ["lahore", "karachi", "kotli", "bhimber", "rawalakot"]
+
+city = input("enter the name of any city in pakistan: ")
+
+if city in list:
+    print("cleanest")
+else:
+    print("dirty city")""""
